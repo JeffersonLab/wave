61d5606b16830a3fd4a347d9f758b1452d453884
ryans <ryans@jlab.org>
2017-07-13 16:23:22 -0400

    Adding timing info

--- web/resources/js/wave.js
+++ web/resources/js/wave.js
@@ -120,114 +120,6 @@ jlab.wave.refreshCanvasJS = function () {
 jlab.wave.addPv = function (pv) {
     jlab.wave.addPvCanvasJS(pv);
 };
-jlab.wave.addPvFlot = function (pv) {
-    if (typeof jlab.wave.pvToChartMap[pv] !== 'undefined') {
-        alert('Already charting pv: ' + pv);
-        return;
-    }
-
-    var $chartHolder = $("#chart-container"),
-            $charts = $chartHolder.find(".chart");
-    if ($charts.length + 1 > jlab.wave.MAX_CHARTS) {
-        alert('Too many charts; maximum number is: ' + jlab.wave.MAX_CHARTS);
-        return;
-    }
-
-    var $div = $('<div class="chart" data-pv="' + pv + '"><div class="chart-title-bar"><span class="chart-title">' + pv + '</span><button type="button" class="chart-close-button">X</button></div><div class="chart-body"></div></div>');
-    $chartHolder.append($div);
-    jlab.wave.doLayout();
-    var minDate = jlab.wave.startDateAndTime,
-            maxDate = jlab.wave.endDateAndTime;
-    var $placeholder = $div.find(".chart-body"),
-            plot = $.plot($placeholder, [[]], {
-                legend: {
-                    show: false
-                },
-                series: {
-                    lines: {
-                        show: true,
-                        steps: false /*This is busted; we'll do it manually*/
-                    },
-                    points: {
-                        show: false
-                    },
-                    shadowSize: 0 /*Performance better without*/
-                },
-                xaxis: {
-                    show: true,
-                    min: minDate.getTime(),
-                    max: maxDate.getTime(),
-                    mode: "time",
-                    timezone: "browser" /*browser local*/
-                },
-                yaxis: {
-                    show: true,
-                    labelWidth: 75
-                },
-                grid: {
-                    hoverable: true,
-                    autoHighlight: false
-                },
-                tooltip: {
-                    show: true,
-                    lines: true,
-                    content: "<b>%s</b><br/><b>x:</b> %x<br/><b>y:</b> %y",
-                    xDateFormat: "%Y-%m-%d %H:%M:%S"
-                },
-                zoom: {
-                    interactive: false
-                },
-                pan: {
-                    interactive: false,
-                    frameRate: 24
-                },
-                selection: {
-                    /*mode: "x"*/
-                }
-            });
-    $placeholder.on("plotzoom", function (event, plot, args) {
-
-        plot.getOptions().xaxes[0].min = new Date(plot.getOptions().xaxes[0].min);
-        plot.getOptions().xaxes[0].max = new Date(plot.getOptions().xaxes[0].max);
-        jlab.wave.startDateAndTime = plot.getOptions().xaxes[0].min;
-        jlab.wave.endDateAndTime = plot.getOptions().xaxes[0].max;
-        console.log('selection: ' + jlab.wave.startDateAndTime.toLocaleString() + " to " + jlab.wave.endDateAndTime.toLocaleString());
-    });
-    $placeholder.on("plotselected", function (event, ranges) {
-        jlab.wave.startDateAndTime = new Date(ranges.xaxis.from);
-        jlab.wave.endDateAndTime = new Date(ranges.xaxis.to);
-        console.log('selection: ' + jlab.wave.startDateAndTime.toLocaleString() + " to " + jlab.wave.endDateAndTime.toLocaleString());
-        /*jlab.wave.refresh();*/
-
-        plot.getOptions().xaxes[0].min = jlab.wave.startDateAndTime;
-        plot.getOptions().xaxes[0].max = jlab.wave.endDateAndTime;
-        plot.setupGrid();
-        plot.draw();
-        plot.clearSelection();
-    });
-    var c = new jlab.wave.Chart(pv, plot);
-    jlab.wave.pvToChartMap[pv] = c;
-    c.$div = $div;
-    jlab.wave.getData(c);
-    $("#pv-input").val("");
-    $("#chart-container").css("border", "none");
-    var uri = new URI(),
-            queryMap = uri.query(true),
-            pvs = queryMap['pv'] || [],
-            addToUrl = false;
-    if (!Array.isArray(pvs)) {
-        pvs = [pvs];
-    }
-
-    if ($.inArray(pv, pvs) === -1) {
-        addToUrl = true;
-    }
-
-    if (addToUrl) {
-        var url = $.mobile.path.addSearchParams($.mobile.path.getLocation(), {pv: pv});
-        window.history.replaceState({}, 'Add pv: ' + pv, url);
-    }
-};
 jlab.wave.addPvCanvasJS = function (pv) {
     if (typeof jlab.wave.pvToChartMap[pv] !== 'undefined') {
         alert('Already charting pv: ' + pv);
@@ -260,7 +152,7 @@ jlab.wave.addPvCanvasJS = function (pv) {
             valueFormatString: "DD-MMM HH:mm",
             labelAngle: -50
         },
-        data: [{type: "line", xValueType: "dateTime", dataPoints: []}]
+        data: [{type: "line", markerType: "none", xValueType: "dateTime", dataPoints: []}]
     });
     /*chart.render();*/
     var c = new jlab.wave.Chart(pv, chart);
@@ -302,8 +194,14 @@ jlab.wave.getDataCanvasJS = function (c) {
             dataType = "json",
             options = {url: url, type: 'GET', data: data, dataType: dataType, timeout: 30000};
     $.mobile.loading("show", {textVisible: true, theme: "b"});
+
+    options.beforeSend = function () {
+        console.time("fetch"); /*This isn't perfect due to event queue running whenever*/
+    };
+
     var promise = $.ajax(options);
     promise.done(function (json) {
+        console.timeEnd("fetch");
         /*console.log(json);*/
 
         c.metadata = {'datatype': json.datatype, 'datasize': json.datasize, 'sampled': json.sampled, 'count': json.count};
@@ -317,9 +215,16 @@ jlab.wave.getDataCanvasJS = function (c) {
             return;
         }
 
+        var makeStepLine = true;
+
         if (json.sampled === true) {
             c.$div.addClass("sampled-data");
+            makeStepLine = false;
             c.plot.options.title.text = c.pv + ' (Sampled)';
+            //c.plot.options.data[0].type = "scatter";
+            c.plot.options.data[0].lineDashType = "dot";
+            //c.plot.options.data[0].markerType = "square"; //square renders WAY faster than circle
+            //c.plot.options.data[0].color = "red";
         } else {
             c.$div.removeClass("sampled-data");
             c.plot.options.title.text = c.pv;
@@ -327,20 +232,33 @@ jlab.wave.getDataCanvasJS = function (c) {
 
         var formattedData = [],
                 prev = null;
-        for (var i = 0; i < json.data.length; i++) {
-            var record = json.data[i],
-                    timestamp = record.d,
-                    value = parseFloat(record.v), /*Should already be float?*/
-                    point = {x: timestamp, y: value};
-            if (prev !== null) {
-                formattedData.push({x: timestamp, y: prev});
-            }
 
-            formattedData.push(point);
-            prev = value;
+        if (makeStepLine) {
+            for (var i = 0; i < json.data.length; i++) {
+                var record = json.data[i],
+                        timestamp = record.d,
+                        value = parseFloat(record.v), /*Should already be float?*/
+                        point = {x: timestamp, y: value};
+                if (prev !== null) {
+                    formattedData.push({x: timestamp, y: prev});
+                }
+
+                formattedData.push(point);
+                prev = value;
+            }
+        } else { /*Don't step data*/
+            for (var i = 0; i < json.data.length; i++) {
+                var record = json.data[i],
+                        timestamp = record.d,
+                        value = parseFloat(record.v), /*Should already be float?*/
+                        point = {x: timestamp, y: value};
+
+                formattedData.push(point);
+            }
         }
 
-        console.log('stepped points: ' + formattedData.length);
+        console.log('transferred points: ' + json.data.length);
+        console.log('total points (includes steps): ' + formattedData.length);
         /*console.log(flotFormattedData);*/
 
         c.plot.options.data[0].dataPoints = formattedData;
@@ -369,99 +287,18 @@ jlab.wave.getDataCanvasJS = function (c) {
     });
     promise.always(function () {
         $.mobile.loading("hide");
-        console.log("data request complete");
+        /*console.log("data request complete");*/
         jlab.wave.doLayout();
     });
     return promise;
 };
 
-jlab.wave.getDataFlot = function (c) {
-    var url = '/myget/jmyapi-span-data',
-            data = {
-                c: c.pv,
-                b: jlab.wave.toIsoDateTimeString(jlab.wave.startDateAndTime),
-                e: jlab.wave.toIsoDateTimeString(jlab.wave.endDateAndTime),
-                t: '',
-                l: 10000
-            },
-            dataType = "json",
-            options = {url: url, type: 'GET', data: data, dataType: dataType, timeout: 10000};
-    $.mobile.loading("show", {textVisible: true, theme: "b"});
-    var promise = $.ajax(options);
-    promise.done(function (json) {
-        /*console.log(json);*/
-
-        c.metadata = {'datatype': json.datatype, 'datasize': json.datasize, 'sampled': json.sampled, 'count': json.count};
-        if (!(json.datatype === 'DBR_DOUBLE' || json.datatype === 'DBR_FLOAT' || json.datatype === 'DBR_SHORT' || json.datatype === 'DBR_LONG')) {
-            alert('datatype not a number: ' + json.datatype);
-            return;
-        }
-
-        if (json.datasize !== 1) { /*This check is probably unnecessary since only vectors are strings*/
-            alert('datasize not scalar: ' + json.datasize);
-            return;
-        }
-
-        if (json.sampled === true) {
-            c.$div.addClass("sampled-data");
-        } else {
-            c.$div.removeClass("sampled-data");
-        }
-
-        var flotFormattedData = [],
-                prev = null;
-        for (var i = 0; i < json.data.length; i++) {
-            var record = json.data[i],
-                    timestamp = record.d,
-                    value = parseFloat(record.v), /*Should already be float?*/
-                    point = [timestamp, value];
-            if (prev !== null) {
-                flotFormattedData.push([timestamp, prev]);
-            }
-
-            flotFormattedData.push(point);
-            prev = value;
-        }
-
-        console.log('stepped points: ' + flotFormattedData.length);
-        /*console.log(flotFormattedData);*/
-
-        console.time("draw");
-        c.data = {label: c.pv, data: flotFormattedData};
-        c.plot.setData([c.data]);
-        c.plot.setupGrid();
-        c.plot.draw();
-        console.timeEnd("draw");
-    });
-    promise.error(function (xhr, t, m) {
-        var json;
-        try {
-            if (t === "timeout") {
-                json = {error: 'Timeout while waiting for response'};
-            } else if (typeof xhr.responseText === 'undefined' || xhr.responseText === '') {
-                json = {};
-            } else {
-                json = $.parseJSON(xhr.responseText);
-            }
-        } catch (err) {
-            window.console && console.log('Response is not JSON: ' + xhr.responseText);
-            json = {};
-        }
-
-        var message = json.error || 'Server did not handle request';
-        alert('Unable to perform request: ' + message);
-    });
-    promise.always(function () {
-        $.mobile.loading("hide");
-    });
-    return promise;
-};
 jlab.wave.doLayout = function () {
     var $chartHolder = $("#chart-container"),
             $charts = $chartHolder.find(".chart");
     var offset = 0;
 
-    console.log("doLayout: chartHolder Height: " + $chartHolder.height());
+    /*console.log("doLayout: chartHolder Height: " + $chartHolder.height());*/
 
     $charts.each(function () {
         var chartHeight = $chartHolder.height() / $charts.length;
