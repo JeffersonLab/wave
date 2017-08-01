e023570d75721e4640687e69639451979bc54c9d
ryans <ryans@jlab.org>
2017-08-01 13:43:34 -0400

    Use 'revealing module' organization

--- web/resources/js/src/util.js
+++ web/resources/js/src/util.js
@@ -1,85 +1,88 @@
-jlab = jlab || {};
-jlab.wave = jlab.wave || {};
-jlab.wave.util = jlab.wave.util || {};
+/*Organized as a 'Revealing Module' with namespace jlab.wave.util*/
+(function (jlab) {
+    (function (wave) {
+        (function (util) {
+            util.triCharMonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            util.fullMonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-jlab.wave.util.triCharMonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-jlab.wave.util.fullMonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            util.hasTouch = function () {
+                try {
+                    document.createEvent("TouchEvent");
+                    return true;
+                } catch (e) {
+                    return false;
+                }
+            };
+            util.pad = function (n, width, z) {
+                z = z || '0';
+                n = n + '';
+                return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+            };
+            util.intToStringWithCommas = function (x) {
+                return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            };
+            util.toIsoDateTimeString = function (x) {
+                let year = x.getFullYear(),
+                        month = x.getMonth() + 1,
+                        day = x.getDate(),
+                        hour = x.getHours(),
+                        minute = x.getMinutes(),
+                        second = x.getSeconds();
+                return year + '-' + util.pad(month, 2) + '-' + util.pad(day, 2) + ' ' + util.pad(hour, 2) + ':' + util.pad(minute, 2) + ':' + util.pad(second, 2);
+            };
+            util.parseIsoDateTimeString = function (x) {
+                let year = parseInt(x.substring(0, 4)),
+                        month = parseInt(x.substring(5, 7)) - 1,
+                        day = parseInt(x.substring(8, 10)),
+                        hour = parseInt(x.substring(11, 13)),
+                        minute = parseInt(x.substring(14, 16)),
+                        second = parseInt(x.substring(17, 19));
+                return new Date(year, month, day, hour, minute, second);
+            };
+            util.toUserDateString = function (x) {
+                let year = x.getFullYear(),
+                        month = x.getMonth(),
+                        day = x.getDate();
+                return util.triCharMonthNames[month] + ' ' + util.pad(day, 2) + ' ' + year;
+            };
+            util.toUserTimeString = function (x) {
+                let hour = x.getHours(),
+                        minute = x.getMinutes(),
+                        second = x.getSeconds();
+                return util.pad(hour, 2) + ':' + util.pad(minute, 2) + ':' + util.pad(second, 2);
+            };
+            util.toUserDateTimeString = function (x) {
+                let year = x.getFullYear(),
+                        month = x.getMonth(),
+                        day = x.getDate(),
+                        hour = x.getHours(),
+                        minute = x.getMinutes(),
+                        second = x.getSeconds();
+                return util.triCharMonthNames[month] + ' ' + util.pad(day, 2) + ' ' + year + ' ' + util.pad(hour, 2) + ':' + util.pad(minute, 2) + ':' + util.pad(second, 2);
+            };
+            util.parseUserDate = function (x) {
+                let month = jlab.wave.util.triCharMonthNames.indexOf(x.substring(0, 3)),
+                        day = parseInt(x.substring(4, 6)),
+                        year = parseInt(x.substring(7, 11));
+                return new Date(year, month, day, 0, 0);
+            };
+            util.parseUserTime = function (x) {
 
-jlab.wave.util.hasTouch = function () {
-    try {
-        document.createEvent("TouchEvent");
-        return true;
-    } catch (e) {
-        return false;
-    }
-};
-jlab.wave.util.pad = function (n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-};
-jlab.wave.util.intToStringWithCommas = function (x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-jlab.wave.util.toIsoDateTimeString = function (x) {
-    let year = x.getFullYear(),
-            month = x.getMonth() + 1,
-            day = x.getDate(),
-            hour = x.getHours(),
-            minute = x.getMinutes(),
-            second = x.getSeconds();
-    return year + '-' + jlab.wave.util.pad(month, 2) + '-' + jlab.wave.util.pad(day, 2) + ' ' + jlab.wave.util.pad(hour, 2) + ':' + jlab.wave.util.pad(minute, 2) + ':' + jlab.wave.util.pad(second, 2);
-};
-jlab.wave.util.parseIsoDateTimeString = function (x) {
-    let year = parseInt(x.substring(0, 4)),
-            month = parseInt(x.substring(5, 7)) - 1,
-            day = parseInt(x.substring(8, 10)),
-            hour = parseInt(x.substring(11, 13)),
-            minute = parseInt(x.substring(14, 16)),
-            second = parseInt(x.substring(17, 19));
-    return new Date(year, month, day, hour, minute, second);
-};
-jlab.wave.util.toUserDateString = function (x) {
-    let year = x.getFullYear(),
-            month = x.getMonth(),
-            day = x.getDate();
-    return jlab.wave.util.triCharMonthNames[month] + ' ' + jlab.wave.util.pad(day, 2) + ' ' + year;
-};
-jlab.wave.util.toUserTimeString = function (x) {
-    let hour = x.getHours(),
-            minute = x.getMinutes(),
-            second = x.getSeconds();
-    return jlab.wave.util.pad(hour, 2) + ':' + jlab.wave.util.pad(minute, 2) + ':' + jlab.wave.util.pad(second, 2);
-};
-jlab.wave.util.toUserDateTimeString = function (x) {
-    let year = x.getFullYear(),
-            month = x.getMonth(),
-            day = x.getDate(),
-            hour = x.getHours(),
-            minute = x.getMinutes(),
-            second = x.getSeconds();
-    return jlab.wave.util.triCharMonthNames[month] + ' ' + jlab.wave.util.pad(day, 2) + ' ' + year + ' ' + jlab.wave.util.pad(hour, 2) + ':' + jlab.wave.util.pad(minute, 2) + ':' + jlab.wave.util.pad(second, 2);
-};
-jlab.wave.util.parseUserDate = function (x) {
-    let month = jlab.wave.util.triCharMonthNames.indexOf(x.substring(0, 3)),
-            day = parseInt(x.substring(4, 6)),
-            year = parseInt(x.substring(7, 11));
-    return new Date(year, month, day, 0, 0);
-};
-jlab.wave.util.parseUserTime = function (x) {
+                let hour, minute, second;
 
-    let hour, minute, second;
+                if (x.trim() === '') {
+                    hour = 0;
+                    minute = 0;
+                    second = 0;
+                } else {
+                    hour = parseInt(x.substring(0, 2));
+                    minute = parseInt(x.substring(3, 5));
+                    second = parseInt(x.substring(6, 9));
+                }
 
-    if (x.trim() === '') {
-        hour = 0;
-        minute = 0;
-        second = 0;
-    } else {
-        hour = parseInt(x.substring(0, 2));
-        minute = parseInt(x.substring(3, 5));
-        second = parseInt(x.substring(6, 9));
-    }
-
-    return new Date(2000, 0, 1, hour, minute, second);
-};
+                return new Date(2000, 0, 1, hour, minute, second);
+            };
+        })(wave.util || (wave.util = {}));
+    })(jlab.wave || (jlab.wave = {}));
+})(jlab || (jlab = {}));
--- web/resources/js/src/zoomabletimeformatter.js
+++ web/resources/js/src/zoomabletimeformatter.js
@@ -1,208 +1,209 @@
-jlab = jlab || {};
-jlab.wave = jlab.wave || {};
+/*Organized as a 'Revealing Module' with namespace jlab.wave*/
+(function (jlab) {
+    (function (wave) {
+        /**
+         * Constructor for ZoomableTimeFormatter object. 
+         * 
+         * A wave ZoomableTimeFormatter encapsulates all of the tasks associated with 
+         * formatting time for a chart that can be zoomed and reset. In particular the 
+         * following public attributes are provided:
+         * 
+         * title - chart title is a concisely formatted unzoomed date range
+         * 
+         * tickFormat - tick format is as concise as possible and avoids repeating 
+         * fields implied by title, but takes into consideration zoom level
+         * 
+         * interval - CanvasJS specific: a sensible interval between ticks given the 
+         * start and end date and zoom
+         * 
+         * intervalType -CanvasJS specific: a sensible scale for the interval "month", 
+         * "week", "day", etc.
+         * 
+         * In addition there are non-changing "starting" values for tickFormat, 
+         * interval, and intervalType such that a reset is possible (zoom out).
+         * 
+         * @param {date} start - The start date
+         * @param {date} end - The end date
+         * @returns {jlab.wave.TimeFormatter} - The time formatter
+         */
+        wave.ZoomableTimeFormatter = function (start, end) {
+            let sameYear = false,
+                    sameMonth = false,
+                    sameDay = false,
+                    oneDaySpecial = false,
+                    oneMonthSpecial = false,
+                    oneYearSpecial = false,
+                    startTimeNonZero = false,
+                    endTimeNonZero = false,
+                    formattedTime = '',
+                    formattedStartDate,
+                    formattedEndDate;
 
-/**
- * Constructor for ZoomableTimeFormatter object. 
- * 
- * A wave ZoomableTimeFormatter encapsulates all of the tasks associated with 
- * formatting time for a chart that can be zoomed and reset. In particular the 
- * following public attributes are provided:
- * 
- * title - chart title is a concisely formatted unzoomed date range
- * 
- * tickFormat - tick format is as concise as possible and avoids repeating 
- * fields implied by title, but takes into consideration zoom level
- * 
- * interval - CanvasJS specific: a sensible interval between ticks given the 
- * start and end date and zoom
- * 
- * intervalType -CanvasJS specific: a sensible scale for the interval "month", 
- * "week", "day", etc.
- * 
- * In addition there are non-changing "starting" values for tickFormat, 
- * interval, and intervalType such that a reset is possible (zoom out).
- * 
- * @param {date} start - The start date
- * @param {date} end - The end date
- * @returns {jlab.wave.TimeFormatter} - The time formatter
- */
-jlab.wave.ZoomableTimeFormatter = function (start, end) {
-    let sameYear = false,
-            sameMonth = false,
-            sameDay = false,
-            oneDaySpecial = false,
-            oneMonthSpecial = false,
-            oneYearSpecial = false,
-            startTimeNonZero = false,
-            endTimeNonZero = false,
-            formattedTime = '',
-            formattedStartDate,
-            formattedEndDate;
+            this.title = '';
+            this.tickFormat = null;
+            this.interval = null;
+            this.intervalType = null;
 
-    this.title = '';
-    this.tickFormat = null;
-    this.interval = null;
-    this.intervalType = null;
-
-    if (start.getHours() !== 0 || start.getMinutes() !== 0 || start.getSeconds() !== 0) {
-        startTimeNonZero = true;
-    }
-
-    if (end.getHours() !== 0 || end.getMinutes() !== 0 || end.getSeconds() !== 0) {
-        endTimeNonZero = true;
-    }
-
-    if (startTimeNonZero || endTimeNonZero) {
-        formattedTime = ' (' + jlab.wave.util.toUserTimeString(start) + ' - ' + jlab.wave.util.toUserTimeString(end) + ')';
-    } else { /*Check for no-time special cases*/
-        let d = new Date(start.getTime());
-        d.setDate(start.getDate() + 1);
-        oneDaySpecial = d.getTime() === end.getTime();
-
-        if (!oneDaySpecial && start.getDate() === 1) { /*Check for one month special*/
-            d = new Date(start.getTime());
-            d.setMonth(start.getMonth() + 1);
-            oneMonthSpecial = d.getTime() === end.getTime();
-
-            if (!oneMonthSpecial && start.getMonth() === 0) { /*Check for one year special*/
-                d = new Date(start.getTime());
-                d.setFullYear(start.getFullYear() + 1);
-                oneYearSpecial = d.getTime() === end.getTime();
+            if (start.getHours() !== 0 || start.getMinutes() !== 0 || start.getSeconds() !== 0) {
+                startTimeNonZero = true;
             }
-        }
-    }
 
-    sameYear = start.getFullYear() === end.getFullYear();
-    sameMonth = sameYear ? start.getMonth() === end.getMonth() : false;
-    sameDay = sameMonth ? start.getDate() === end.getDate() : false;
+            if (end.getHours() !== 0 || end.getMinutes() !== 0 || end.getSeconds() !== 0) {
+                endTimeNonZero = true;
+            }
 
-    if (oneDaySpecial) {
-        this.title = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getDate() + ', ' + start.getFullYear();
-    } else if (oneMonthSpecial) {
-        this.title = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getFullYear();
-    } else if (oneYearSpecial) {
-        this.title = start.getFullYear();
-    } else {
-        if (sameYear) {
-            formattedStartDate = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getDate();
+            if (startTimeNonZero || endTimeNonZero) {
+                formattedTime = ' (' + jlab.wave.util.toUserTimeString(start) + ' - ' + jlab.wave.util.toUserTimeString(end) + ')';
+            } else { /*Check for no-time special cases*/
+                let d = new Date(start.getTime());
+                d.setDate(start.getDate() + 1);
+                oneDaySpecial = d.getTime() === end.getTime();
 
-            if (sameMonth) {
-                if (sameDay) {
-                    formattedEndDate = ', ' + end.getFullYear();
-                } else { /*Days differ*/
-                    formattedEndDate = ' - ' + end.getDate() + ', ' + end.getFullYear();
+                if (!oneDaySpecial && start.getDate() === 1) { /*Check for one month special*/
+                    d = new Date(start.getTime());
+                    d.setMonth(start.getMonth() + 1);
+                    oneMonthSpecial = d.getTime() === end.getTime();
+
+                    if (!oneMonthSpecial && start.getMonth() === 0) { /*Check for one year special*/
+                        d = new Date(start.getTime());
+                        d.setFullYear(start.getFullYear() + 1);
+                        oneYearSpecial = d.getTime() === end.getTime();
+                    }
                 }
-            } else { /*Months differ*/
-                formattedEndDate = ' - ' + jlab.wave.util.fullMonthNames[end.getMonth()] + ' ' + end.getDate() + ', ' + end.getFullYear();
             }
-        } else { /*Years differ*/
-            formattedStartDate = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getDate() + ', ' + start.getFullYear();
-            formattedEndDate = ' - ' + jlab.wave.util.fullMonthNames[end.getMonth()] + ' ' + end.getDate() + ', ' + end.getFullYear();
-        }
 
-        this.title = formattedStartDate + formattedEndDate + formattedTime;
-    }
+            sameYear = start.getFullYear() === end.getFullYear();
+            sameMonth = sameYear ? start.getMonth() === end.getMonth() : false;
+            sameDay = sameMonth ? start.getDate() === end.getDate() : false;
 
-    let impliedYear = sameYear || oneYearSpecial,
-            impliedYearMonth = sameMonth || oneMonthSpecial,
-            impliedYearMonthDay = sameDay || oneDaySpecial;
+            if (oneDaySpecial) {
+                this.title = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getDate() + ', ' + start.getFullYear();
+            } else if (oneMonthSpecial) {
+                this.title = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getFullYear();
+            } else if (oneYearSpecial) {
+                this.title = start.getFullYear();
+            } else {
+                if (sameYear) {
+                    formattedStartDate = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getDate();
 
-    /**
-     * Adjusts the tickFormat, interval, and intervalType given zoom parameters.
-     * 
-     * @param {number} minMillis - starting miliseconds from Epoch
-     * @param {number} maxMillis - ending milliseconds from Epoch
-     */
-    jlab.wave.ZoomableTimeFormatter.prototype.adjustForViewportZoom = function (minMillis, maxMillis) {
-        let formatter = {year: false, month: false, day: false, hour: false, minute: false, second: false};
+                    if (sameMonth) {
+                        if (sameDay) {
+                            formattedEndDate = ', ' + end.getFullYear();
+                        } else { /*Days differ*/
+                            formattedEndDate = ' - ' + end.getDate() + ', ' + end.getFullYear();
+                        }
+                    } else { /*Months differ*/
+                        formattedEndDate = ' - ' + jlab.wave.util.fullMonthNames[end.getMonth()] + ' ' + end.getDate() + ', ' + end.getFullYear();
+                    }
+                } else { /*Years differ*/
+                    formattedStartDate = jlab.wave.util.fullMonthNames[start.getMonth()] + ' ' + start.getDate() + ', ' + start.getFullYear();
+                    formattedEndDate = ' - ' + jlab.wave.util.fullMonthNames[end.getMonth()] + ' ' + end.getDate() + ', ' + end.getFullYear();
+                }
 
-        this.intervalType = null;
-        this.interval = null;
+                this.title = formattedStartDate + formattedEndDate + formattedTime;
+            }
 
-        if (!impliedYear) {
-            formatter.year = true;
+            let impliedYear = sameYear || oneYearSpecial,
+                    impliedYearMonth = sameMonth || oneMonthSpecial,
+                    impliedYearMonthDay = sameDay || oneDaySpecial;
 
-            if (!impliedYearMonth) {
-                formatter.month = true;
+            /**
+             * Adjusts the tickFormat, interval, and intervalType given zoom parameters.
+             * 
+             * @param {number} minMillis - starting miliseconds from Epoch
+             * @param {number} maxMillis - ending milliseconds from Epoch
+             */
+            wave.ZoomableTimeFormatter.prototype.adjustForViewportZoom = function (minMillis, maxMillis) {
+                let formatter = {year: false, month: false, day: false, hour: false, minute: false, second: false};
 
-                if (!impliedYearMonthDay) {
+                this.intervalType = null;
+                this.interval = null;
+
+                if (!impliedYear) {
+                    formatter.year = true;
+
+                    if (!impliedYearMonth) {
+                        formatter.month = true;
+
+                        if (!impliedYearMonthDay) {
+                            formatter.day = true;
+                        }
+                    }
+                }
+
+                let millisPerMinute = 1000 * 60, /*Ignore leap seconds as timestamps from Epoch do*/
+                        millisPerHour = millisPerMinute * 60,
+                        millisPerDay = millisPerHour * 24, /*UTC - no timezone - no daylight savings*/
+                        millisPerMonth = millisPerDay * 30, /*Approximate*/
+                        millisPerYear = millisPerMonth * 12,
+                        rangeMillis = (maxMillis - minMillis);
+
+                // Less than a few minutes
+                if ((rangeMillis / millisPerMinute) < 5) {
+                    formatter.hour = true;
+                    formatter.minute = true;
+                    formatter.second = true;
+                }
+                // Less than a few hours
+                else if ((rangeMillis / millisPerHour) < 12) {
+                    formatter.hour = true;
+                    formatter.minute = true;
+                }
+                // Less than a few days
+                else if (rangeMillis / (millisPerDay) < 7) {
+                    formatter.hour = true;
+                    /*this.interval = Math.max(Math.round((rangeMillis / millisPerHour) / 12), 1);
+                     this.intervalType = 'hour';*/
+                }
+                // Less than a few months
+                else if ((rangeMillis / millisPerMonth) < 6) {
                     formatter.day = true;
                 }
-            }
-        }
+                // Less than a few years
+                else if ((rangeMillis / millisPerYear) < 3) {
+                    formatter.month = true;
+                    formatter.day = false;
+                    this.interval = Math.max(Math.round((rangeMillis / millisPerMonth) / 12), 1);
+                    this.intervalType = 'month';
+                }
+                // Many years
+                else {
+                    formatter.year = true;
+                    formatter.month = true;
+                    formatter.day = false;
+                }
 
-        let millisPerMinute = 1000 * 60, /*Ignore leap seconds as timestamps from Epoch do*/
-                millisPerHour = millisPerMinute * 60,
-                millisPerDay = millisPerHour * 24, /*UTC - no timezone - no daylight savings*/
-                millisPerMonth = millisPerDay * 30, /*Approximate*/
-                millisPerYear = millisPerMonth * 12,
-                rangeMillis = (maxMillis - minMillis);
+                this.tickFormat = '';
 
-        // Less than a few minutes
-        if ((rangeMillis / millisPerMinute) < 5) {
-            formatter.hour = true;
-            formatter.minute = true;
-            formatter.second = true;
-        }
-        // Less than a few hours
-        else if ((rangeMillis / millisPerHour) < 12) {
-            formatter.hour = true;
-            formatter.minute = true;
-        }
-        // Less than a few days
-        else if (rangeMillis / (millisPerDay) < 7) {
-            formatter.hour = true;
-            /*this.interval = Math.max(Math.round((rangeMillis / millisPerHour) / 12), 1);
-             this.intervalType = 'hour';*/
-        }
-        // Less than a few months
-        else if ((rangeMillis / millisPerMonth) < 6) {
-            formatter.day = true;
-        }
-        // Less than a few years
-        else if ((rangeMillis / millisPerYear) < 3) {
-            formatter.month = true;
-            formatter.day = false;
-            this.interval = Math.max(Math.round((rangeMillis / millisPerMonth) / 12), 1);
-            this.intervalType = 'month';
-        }
-        // Many years
-        else {
-            formatter.year = true;
-            formatter.month = true;
-            formatter.day = false;
-        }
+                if (formatter.month) {
+                    this.tickFormat = this.tickFormat + "MMM";
+                }
 
-        this.tickFormat = '';
+                if (formatter.day) {
+                    this.tickFormat = this.tickFormat + " DD";
+                }
 
-        if (formatter.month) {
-            this.tickFormat = this.tickFormat + "MMM";
-        }
+                if (formatter.year) {
+                    this.tickFormat = this.tickFormat + " YYYY";
+                }
 
-        if (formatter.day) {
-            this.tickFormat = this.tickFormat + " DD";
-        }
+                if (formatter.hour || formatter.minute) {
+                    this.tickFormat = this.tickFormat + " HH:mm";
+                }
 
-        if (formatter.year) {
-            this.tickFormat = this.tickFormat + " YYYY";
-        }
+                if (formatter.second) {
+                    this.tickFormat = this.tickFormat + ":ss";
+                }
 
-        if (formatter.hour || formatter.minute) {
-            this.tickFormat = this.tickFormat + " HH:mm";
-        }
+                this.tickFormat = this.tickFormat.trim();
+            };
 
-        if (formatter.second) {
-            this.tickFormat = this.tickFormat + ":ss";
-        }
+            /*Initialize with unzoomed values*/
+            this.adjustForViewportZoom(start.getTime(), end.getTime());
 
-        this.tickFormat = this.tickFormat.trim();
-    };
-
-    /*Initialize with unzoomed values*/
-    this.adjustForViewportZoom(start.getTime(), end.getTime());
-
-    this.startingTickFormat = this.tickFormat;
-    this.startingInterval = this.interval;
-    this.startingIntervalType = this.intervalType;
-};
-
+            this.startingTickFormat = this.tickFormat;
+            this.startingInterval = this.interval;
+            this.startingIntervalType = this.intervalType;
+        };
+    })(jlab.wave || (jlab.wave = {}));
+})(jlab || (jlab = {}));
