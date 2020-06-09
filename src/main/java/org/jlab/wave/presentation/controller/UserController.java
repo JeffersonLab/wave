7b36a389f64d68c726b7b00b7731875a6c811fb5
ryans <ryans@jlab.org>
2020-06-09 14:00:17 -0400

    Exploring remote user

---
+++ src/main/java/org/jlab/wave/presentation/controller/UserController.java
@@ -0,0 +1,31 @@
+package org.jlab.wave.presentation.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author ryans
+ */
+@WebServlet(name = "UserController", urlPatterns = {"/user"})
+public class UserController extends HttpServlet {
+    
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.getRequestDispatcher("/WEB-INF/views/user.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/user.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<!DOCTYPE html>
+<html class="${param.print eq 'Y' ? 'print ' : ''}">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>WAVE</title>
+        <link rel="shortcut icon" href="${pageContext.request.contextPath}/resources/img/favicon.ico?v=${initParam.releaseNumber}"/>
+    </head>
+    <body>
+    <p>Remote User: <c:out value="${pageContext.request.remoteUser}"/></p>
+    </body>
+</html>
